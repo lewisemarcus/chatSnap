d8d5b25807c18d38132c6e99e6f98c23f60b8671
Marcus Lewis <72634924+lewisemarcus@users.noreply.github.com>
2022-08-06 02:27:09 -0700

    substituted real data for dummy data for home screen

--- client/components/ChatRoomItem/ChatRoomItem.js
+++ client/components/ChatRoomItem/ChatRoomItem.js
@@ -1,41 +1,50 @@
-import React from "react"
+import React, { useContext } from "react"
 import { Text, View, Image, Pressable } from "react-native"
 import { useNavigation } from "@react-navigation/core"
 import styles from "./styles/ChatRoomStyle"
 
 export default function ChatRoomItem({ chatRoom }) {
-    const user = chatRoom.users[1]
     const navigation = useNavigation()
     const onPress = () => {
-        navigation.navigate("ChatRoom", { id: chatRoom.id })
+        navigation.navigate("ChatRoom", { id: chatRoom.uid })
     }
-
+    console.log(chatRoom.userEmails)
     return (
         <Pressable style={styles.container} onPress={onPress}>
-            <Image
+            {/* TODO: add image upload function for users on settings page, add default image */}
+            {/* <Image
                 source={{
                     uri: user.imageUri,
                 }}
                 style={styles.image}
-            />
+            /> */}
 
             {chatRoom.newMessages && (
                 <View style={styles.badgeContainer}>
                     <Text style={styles.badgeText}>{chatRoom.newMessages}</Text>
                 </View>
             )}
-
-            <View style={styles.rightContainer}>
-                <View style={styles.row}>
-                    <Text style={styles.name}>{user.name}</Text>
-                    <Text style={styles.text}>
-                        {chatRoom.lastMessage.createdAt}
+            {chatRoom && chatRoom.userEmails && (
+                <View style={styles.rightContainer}>
+                    <View style={styles.row}>
+                        <Text style={styles.name}>
+                            {chatRoom.userEmails[0]}
+                        </Text>
+                        <Text style={styles.text}>
+                            {
+                                chatRoom.messages[chatRoom.messages.length - 1]
+                                    .createdAt.$date
+                            }
+                        </Text>
+                    </View>
+                    <Text numberOfLines={1} style={styles.text}>
+                        {
+                            chatRoom.messages[chatRoom.messages.length - 1]
+                                .message
+                        }
                     </Text>
                 </View>
-                <Text numberOfLines={1} style={styles.text}>
-                    {chatRoom.lastMessage.content}
-                </Text>
-            </View>
+            )}
         </Pressable>
     )
 }
--- client/screens/HomeScreen.js
+++ client/screens/HomeScreen.js
@@ -2,19 +2,19 @@ import { StyleSheet, Text, View, FlatList } from "react-native"
 import { useContext, useEffect, useState } from "react"
 
 import ChatRoomItem from "../components/ChatRoomItem/ChatRoomItem"
-import chatRoomsData from "../assets/dummy-data/ChatRooms"
+
 import { AuthContext } from "../context/AuthContext"
 import { NewMessageBtn } from "../components/NewMessageBtn/NewMessageBtn"
 
 export default function Home({ navigation }) {
-    const [users, setUsers] = useState([])
-    const { instance, user } = useContext(AuthContext)
+    const { user } = useContext(AuthContext)
     //TODO: replace chatRoomsData with user.chatrooms
+
     return (
         <View style={styles.container}>
             {/* FlatList enables scrollability */}
             <FlatList
-                data={chatRoomsData}
+                data={user.chatrooms}
                 // rename's the item from props to eachChatRoom
                 renderItem={({ item: eachChatRoom }) => (
                     <ChatRoomItem chatRoom={eachChatRoom} />
